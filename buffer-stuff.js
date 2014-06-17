356cc26bddb349e01430197f7aca5658c2d3606f
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-06-16 23:41:01 -0300

    Added exercises 2 and 3 solutions.

--- buffer-stuff.js
+++ buffer-stuff.js
@@ -1,7 +1,23 @@
-exports.exercise_1 = (function () {
+exports.exercise_1 = function () {
   var buffer = initializeBuffer();
   printBuffer(buffer);
-})();
+};
+
+exports.exercise_2 = function () {
+  var buffer = initializeBuffer();
+  var slice = buffer.slice(40, 60);
+  printBuffer(slice);
+};
+
+exports.exercise_3 = function () {
+  var buffer = initializeBuffer(),
+      slice = new Buffer(20),
+      targetStart = 0,
+      sourceStart = 40,
+      sourceEnd = 60;
+  buffer.copy(slice, targetStart, sourceStart, sourceEnd);
+  printBuffer(slice);
+};
 
 function initializeBuffer () {
   var buffer = new Buffer(100);
