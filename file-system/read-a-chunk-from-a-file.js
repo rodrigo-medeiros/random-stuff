9e5158c9080af517e607b6413bbeead21fd4266c
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-06-26 00:00:22 -0300

    Renamed internal function.

--- file-system/read-a-chunk-from-a-file.js
+++ file-system/read-a-chunk-from-a-file.js
@@ -8,7 +8,7 @@ fs.open('a.txt', 'r', function (err, fd) {
   var readBuffer = new Buffer(5),
       bytesRead = 0;
 
-  (function readBytes () {
+  (function printBytes() {
     fs.read(fd, readBuffer, bytesRead, readBuffer.length - bytesRead, 10 + bytesRead, function (err, bytes) {
       if (err) {
         console.log(err);
@@ -18,7 +18,7 @@ fs.open('a.txt', 'r', function (err, fd) {
       if (bytesRead == readBuffer.length) {
         console.log(readBuffer.toString());
       } else {
-        readBytes();
+        printBytes();
       }
     });
   })();
