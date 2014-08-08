59b6162ab528db7dce2e92b3f775e9e850b4057b
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-08 00:34:15 -0300

    Improved, but not finished.

--- validation_test/app.js
+++ validation_test/app.js
@@ -1,4 +1,5 @@
 var express = require('express'),
+    util = require('util'),
     bodyParser = require('body-parser'),
     expressValidator = require('express-validator'),
     app = express();
@@ -7,8 +8,19 @@ app.use(bodyParser.json());
 app.use(expressValidator());
 
 app.post('/', function (req, res) {
-  console.log(req.body);
-  res.send(req.body);
+
+  req.checkBody('name', 'Missing params...').notEmpty();
+  req.checkBody(['products'[0], 'name'], 'Name is required').notEmpty();
+
+  var errors = req.validationErrors();
+  console.log(errors);
+
+  if (errors) {
+    res.status(200).send('There have been validation errors: ' + util.inspect(errors));
+    return;
+  }
+  
+  res.json(req.body);
 });
 
 var server = app.listen(3000, function () {
