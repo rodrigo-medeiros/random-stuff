a9b29c50e969d08f48a6345bec9ae3b67d66bd56
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-09 00:41:36 -0300

    Not working the way I expect yet.

--- validation_test/app.js
+++ validation_test/app.js
@@ -10,17 +10,17 @@ app.use(expressValidator());
 app.post('/', function (req, res) {
 
   req.checkBody('name', 'Missing params...').notEmpty();
-  req.checkBody(['products'[0], 'name'], 'Name is required').notEmpty();
+  req.checkBody('products'[0], 'Name is required').notEmpty();
 
   var errors = req.validationErrors();
-  console.log(errors);
 
   if (errors) {
     res.status(200).send('There have been validation errors: ' + util.inspect(errors));
     return;
   }
-  
-  res.json(req.body);
+  res.json({
+    name: req.name
+  });
 });
 
 var server = app.listen(3000, function () {
