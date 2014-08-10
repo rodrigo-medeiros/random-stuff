50052a8bdb8da7d8fda2c493c2ac0d15e31228ed
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-10 01:22:48 -0300

    Helping someone on Stack Overflow.

--- request-test.js
+++ request-test.js
@@ -1,9 +1,5 @@
 var request = require('request');
 
-request('https://coderbits.com/rodrigomangue.json')
-  .on('data', function (data) {
-    console.log(JSON.stringify(data.toString()));
-  })
-  .on('end', function () {
-    console.log('This is the end...');
-  });
+request('http://ancient-falls-7604.herokuapp.com/users/1/posts', function (error, response, body) {
+  console.log(JSON.parse(body).user_posts[0]);
+});
