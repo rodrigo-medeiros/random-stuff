b47d0a225daf74e55b39542111e5d1900dab46ba
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2014-08-14 00:22:38 -0300

    Testing CTRL+C stuff.

--- ctrl_c.js
+++ ctrl_c.js
@@ -1,8 +1,17 @@
-setTimeout(function () {
+var tty = require('tty');
+
+process.stdin.on('keypress', function (chunk, key) {
+  if (key && key.name === 'c' && key.ctrl) {
+    console.log('yeah...');
+    process.exit();
+  }
+});
+
+/*setTimeout(function () {
   console.log('This is the end');
 }, 5000);
 
 process.on('SIGINT', function () {
   console.log('blah');
   process.exit(1);
-});
+});*/
