4f418bdf625049ef5d3ff109e25743d586a74d4e
rodrigomangue <rodrigo.mangue@gmail.com>
2014-12-11 10:56:44 -0200

    Allowed CORS.

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 function makeAjaxRequest () {
-  $.ajax({
-    url: 'http://127.0.0.1:3000/',
-    data: {
-      mydata: "content"
-    },
-    dataType: 'jsonp',
-    success: function (json) {
-      console.log(json);
-    },
-    error: function (jqXHR, error, errorThrown) {
-      console.log(error);
-    },
-    type: 'GET'
-  });
+    $.ajax({
+      url: 'http://127.0.0.1:3000/',
+      data: {
+        mydata: "content"
+      },
+      dataType: 'text',
+      success: function (json) {
+        console.log(json);
+      },
+      error: function (jqXHR, error, errorThrown) {
+        console.log(error);
+      },
+      type: 'GET'
+    });
 }
 
 function makeAjaxGetRequest () {
