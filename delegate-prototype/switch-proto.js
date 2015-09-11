6ce84274098945544caad7b564fb9fb2fd5a6bb2
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2015-09-11 00:24:35 -0300

    Added one more test.

--- delegate-prototype/switch-proto.js
+++ delegate-prototype/switch-proto.js
@@ -10,21 +10,46 @@ var switchProto = {
       return this;
     },
 
+    meta: {
+      name: 'Light switch'
+    },
+
     state: false
   },
   switch1 = Object.create(switchProto),
   switch2 = Object.create(switchProto);
 
-  test('Object create', function (t) {
-    t.ok(
-      switch1.toggle().isOn(),
-      '.toggle() works'
-    );
+test('Object create', function (t) {
+  t.ok(
+    switch1.toggle().isOn(),
+    '.toggle() works'
+  );
 
-    t.ok(
-      !switch2.isOn(),
-      'instance safe'
-    );
+  t.ok(
+    !switch2.isOn(),
+    'instance safe'
+  );
 
-    t.end();
-  });
+  t.end();
+});
+
+test('Prototype mutations', function (t) {
+  switch2.meta.name = 'Breaker switch';
+
+  t.equal(
+    switch1.meta.name,
+    'Breaker switch',
+    'Object and array mutations are shared'
+  );
+
+  switch2.meta = { name: 'Power switch' };
+
+  t.equal(
+    switch1.meta.name,
+    'Breaker switch',
+    'Property replacement is instance-specific.'
+  );
+
+  t.end();
+
+});
