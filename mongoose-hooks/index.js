d240b2e4162aee3a4285a99f62e5a57359ad67bf
Rodrigo Medeiros <rodrigo.mangue@gmail.com>
2016-04-26 23:21:57 -0300

    Added mongoose expamples.

--- mongoose-hooks/index.js
+++ mongoose-hooks/index.js
@@ -1,47 +1,7 @@
-var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
+var mongoose = require("mongoose");
+var url = "mongodb://localhost/hooks-test";
 
-var MySchema = new Schema({
-  name: String,
-  queryCount: {
-    type: Number,
-    default: 0
-  }
-});
-
-MySchema.post('init', function (doc) {
-  doc.queryCount++;
-  doc.save();
-});
-
-var Thing = mongoose.model('Thing', MySchema);
-
-mongoose.connect('mongodb://localhost/hooks-test');
-
-//var thing = new Thing({
-//  name: "blah"
-//});
-//thing.save(function (err, doc) {
-//  if (err) {
-//    throw err;
-//  }
-//});
-
-Thing.find({name: "blah"}, function (err, docs) {
-  if (err) {
-    throw err;
-  }
-  console.log(docs);
-});
-Thing.find({name: "blah"}, function (err, docs) {
-  if (err) {
-    throw err;
-  }
-  console.log(docs);
-});
-Thing.find({name: "blah"}, function (err, docs) {
-  if (err) {
-    throw err;
-  }
-  console.log(docs);
-});
+mongoose.connect(url);
+console.log("isonerror");
+var mydb = mongoose.connection;
+console.log("ismydb");
--- mongoose-tests/server1.js
+++ mongoose-tests/server1.js
@@ -1,19 +1,19 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Course = require('./schemas/course');
+var express = require("express");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+var Course = require("./schemas/course");
 
 var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-mongoose.connect('mongodb://localhost/school-test');
+mongoose.connect("mongodb://localhost/school-test");
 
-app.get('/courses', function (req, res, next) {
+app.get("/courses", function (req, res, next) {
 
-  Course.find({'disciplines.books.author': "Sofia"}, function (err, courses) {
-    
+  Course.find({"disciplines.books.author": "Sofia"}, function (err, courses) {
+
     if (err) {
       return res.status(500).json({ error: err });
     }
@@ -24,7 +24,7 @@ app.get('/courses', function (req, res, next) {
   });
 });
 
-app.post('/courses', function (req, res, next) {
+app.post("/courses", function (req, res, next) {
 
   var course = new Course(req.body);
   course.save(function (err, courseResponse) {
@@ -45,6 +45,6 @@ var server = app.listen(3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('App waiting for requests at http://%s:%s', host, port);
+  console.log("App waiting for requests at http://%s:%s", host, port);
 
 });
